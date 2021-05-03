3adc9699377c61a11b9aec0553b10e8199a57ff3
Gideon Tong <gideon@gideontong.com>
2021-05-03 00:35:05 -0700

    Complete example

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import email.mime.multipart
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import json
 import smtplib
 import time
@@ -9,13 +12,36 @@ secrets = json.load(open('secrets.json'))
 server = smtplib.SMTP_SSL('smtp.gmail.com')
 server.login(secrets['username'], secrets['password'])
 
+resume = open('resume.pdf', 'rb')
+attachment = resume.read()
+
 for recipient in recipients:
     recipient = recipient.strip()
 
-    message = email.mime.multipart.MIMEMultipart()
+    message = MIMEMultipart()
     message['Subject'] = 'Lab positions'
     message['From'] = secrets['username']
     message['To'] = recipient
+
+    body = """
+    To whom it may concern,
+
+    I'm interested in working in your lab this summer
+    and I have attached my resume.
+
+    Best,
+    Billy Bob Joe
+    """
+
+    message.attach(MIMEText(body, 'plain'))
+
+    filename = 'resume.pdf'
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment)
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename= {filename}')
+
+    message.attach(part)
     
     server.sendmail(secrets['username'], recipient, message.as_string())
     print(f'Succecssfully sent email to {recipient}')
