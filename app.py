7bb325004f680caa3d8ada23420c3070f1ce905a
Gideon Tong <gideon@gideontong.com>
2021-05-03 00:20:59 -0700

    Prepare multipart

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
+import email.mime.multipart
 import json
 import smtplib
 import time
 
-recipients = open("contacts.txt").readlines()
-secrets = json.load(open("secrets.json"))
+recipients = open('contacts.txt').readlines()
+secrets = json.load(open('secrets.json'))
 
-server = smtplib.SMTP_SSL("smtp.gmail.com")
-server.login(secrets["username"], secrets["password"])
+server = smtplib.SMTP_SSL('smtp.gmail.com')
+server.login(secrets['username'], secrets['password'])
 
 for recipient in recipients:
     recipient = recipient.strip()
 
-    email = f"""From: {secrets["username"]}
-    To: {recipient}
-    Subject: Test email
-
-    Hello world from our first email!
-    """
-
-    # server.sendmail(secrets["username"], recipient, email)
-    print(f"Succecssfully sent email to {recipient}")
+    message = email.mime.multipart.MIMEMultipart()
+    message['Subject'] = 'Lab positions'
+    message['From'] = secrets['username']
+    message['To'] = recipient
+    
+    server.sendmail(secrets['username'], recipient, message.as_string())
+    print(f'Succecssfully sent email to {recipient}')
 
     time.sleep(15)
 
